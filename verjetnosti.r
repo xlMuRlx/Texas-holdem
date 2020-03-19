520ca9e0e7460f69a96c8ddaf3bb5af19f19c0b2
xlMuRlx <andraz.mur44@gmail.com>
2020-03-19 16:34:37 +0100

    Začel s projektom

---
+++ verjetnosti.r
@@ -0,0 +1,16 @@
+# Najprej sestavimo seznam oz. tabelo vseh kart. Vsaka karta bo predstavljena kot par, kjer prva komponenta pove njeno vrednost, druga pa barvo.
+
+vrednosti <- c()
+for (i in 2:10) {
+  vrednosti[[i-1]] <- as.character(i)
+}
+vrednosti <- c(vrednosti, c("J", "Q", "K", "A"))
+
+barve <- c("kriz", "pik", "srce", "karo")
+
+kupcek <- expand.grid(vrednosti, barve)
+colnames(kupcek) <- c("Vrednost", "Barva")
+
+
+
+
