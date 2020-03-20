439af155436bbb5c0606ca8544f4ee5787b38120
xlMuRlx <andraz.mur44@gmail.com>
2020-03-20 20:09:33 +0100

    Manjši popravki

--- verjetnosti.r
+++ verjetnosti.r
@@ -9,7 +9,36 @@ vrednosti <- c(vrednosti, c("J", "Q", "K", "A"))
 barve <- c("kriz", "pik", "srce", "karo")
 
 kupcek <- expand.grid(vrednosti, barve)
-colnames(kupcek) <- c("Vrednost", "Barva")
+colnames(kupcek) <- c("vrednost", "barva")
+
+
+
+
+
+
+# Potrebujemo neko funkcijo za medsebojno primerjavo različnih kombinacij kart. Gre za funkcijo, ki iz izbranih 7 kart izbere in ovrednoti naboljšo možno kombinacijo.
+
+ovrednoti <- function(kombinacija) {
+  kombinacija$vrednost[kombinacija$vrednost == "J"] <- 11
+  kombinacija$vrednost[kombinacija$vrednost == "Q"] <- 12
+  kombinacija$vrednost[kombinacija$vrednost == "K"] <- 13
+  kombinacija$vrednost[kombinacija$vrednost == "A"] <- 14
+  kombinacija$vrednost <- as.numeric(kombinacija$vrednost)
+  
+  kombinacija <- kombinacija[order(-kombinacija$vrednost), ]
+  
+}
+
+
+
+
+
+# Za lažji zapis shiny-ja
+karte <- paste(kupcek$vrednost, kupcek$barva)
+
+
+
+
 
 
 
